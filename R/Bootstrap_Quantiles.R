0a912dad94564ea7b8986518be60956638d9736c
Fabian <fabian.telschow@hu-berlin.de>
2023-04-19 13:40:30 +0200

    minor modifications in description

--- R/Bootstrap_Quantiles.R
+++ R/Bootstrap_Quantiles.R
@@ -8,7 +8,7 @@
 #      - matrixStats
 #
 # Contained functions:
-#      - MultiplierBootstrapSplit (tested)
+#      - MultiplierBootstrapSplit
 #
 #------------------------------------------------------------------------------#
 # Developer notes:
