1783cd4b0de5dad340a80d573292567f357377a4
Fabian <fabian.telschow@hu-berlin.de>
2023-04-20 13:42:59 +0200

    added t-version of the kac rice formula

--- R/KacRice_Formulas.R
+++ R/KacRice_Formulas.R
@@ -33,22 +33,42 @@ GeneralKacRice_t <- function(tau, u, du, x, df = NULL,
     f0 <- pnorm
 
     f1 <- function(t){
-      tau(t) / (2*pi) * exp( -(u(t)^2 + (du(t)/tau(t))^2) / (2*sigma^2) )
+              tau(t) / (2*pi) * exp( -(u(t)^2 + (du(t)/tau(t))^2) / (2*sigma^2) )
     }
 
     f2 <-  function(t){
-      du(t) / sqrt(2*pi*sigma^2) *
-        exp(-u(t)^2 / (2*sigma^2)) *
-        pnorm(q = du(t) / sigma / tau(t))
+              du(t) / sqrt(2*pi*sigma^2) * exp(-u(t)^2 / (2*sigma^2)) *
+                pnorm(q = du(t) / sigma / tau(t))
     }
 
     f3 <-  function(t){
-      du(t) / sqrt(2*pi*sigma^2) *
-        exp(-u(t)^2 / (2*sigma^2)) *
+      du(t) / sqrt(2*pi*sigma^2) * exp(-u(t)^2 / (2*sigma^2)) *
         pnorm(q = -du(t) / sigma / tau(t))
     }
   }else{
+    nup = df + 1
+    afun <- function(t){ sqrt(nu * tau(t)^2 * (1 + u(t)^2 / df) / nup)}
+
     # Add here the functions for the t-Kac-Rice formula
+    f0 <- function(t){
+        stats::pt(q = -u(t), df = df)
+    }
+
+    f1 <- function(t){
+        tau(t) * (1 + u(t)^2 / df + u(t)^2/(df * tau_f(t)^2))^(-df / 2) / (2*pi)
+    }
+
+    f2 <- function(t){
+      du(t) / (2*pi*tau(t)) * (1 + u(t)^2 / df)^(-df/2 - 1)  *
+        gamma(nup/2) * sqrt(nup*pi) * afun(t) / gamma((nup + 1) / 2) *
+        stats::pt(q = du(t) / afun(t), df = nup)
+    }
+
+    f3 <- function(t){
+      du(t) / (2*pi*tau(t)) * (1 + u(t)^2 / df)^(-df/2 - 1)  *
+        gamma(nup/2) * sqrt(nup*pi) * afun(t) / gamma((nup + 1) / 2) *
+        stats::pt(q = -du(t) / afun(t), df = nup)
+    }
   }
 
   I1 <- f0(-u(x[t0]) / sigma) +
